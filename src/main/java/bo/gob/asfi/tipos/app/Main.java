bdd3b879f3593da13dddaad6b67514556c0bcc96
Fernando Ontiveros <feronti@gmail.com>
2016-10-10 01:14:51 -0400

    random class

--- src/main/java/bo/gob/asfi/tipos/app/Main.java
+++ src/main/java/bo/gob/asfi/tipos/app/Main.java
@@ -1,8 +1,12 @@
-package bo.gob.asfi.tipos;
+package bo.gob.asfi.tipos.app;
+
+import bo.gob.asfi.tipos.utils.Common;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Common.displayTitle("tipos");
 	    System.out.println("hello from Intellij ");
     }
 }
