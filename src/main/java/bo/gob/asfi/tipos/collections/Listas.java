824ab89c8940e9941715e0606cb3d3e04eef151c
Fernando Ontiveros <feronti@gmail.com>
2016-10-12 13:55:20 -0400

    listas

--- src/main/java/bo/gob/asfi/tipos/collections/Listas.java
+++ src/main/java/bo/gob/asfi/tipos/collections/Listas.java
@@ -1,5 +1,9 @@
 package bo.gob.asfi.tipos.collections;
 
+import bo.gob.asfi.tipos.model.Usuario;
+
+import java.util.Date;
+
 /**
  * Created by fernando on 10/12/16.
  */
@@ -17,7 +21,54 @@ public class Listas
 		dataType[] arrayRefVar = {value0, value1, ..., valuek};
 */
 
-		double[] myList = new double[10];
+		Double[] doubleList = {10.0, 20.0, 30.0, 40.0, 50.0, 6.0, 70.0, 80.0, 100.0};
+
+		Integer[] intList = {10, 20, 30, 40, 50, 60, 70, 80, 100};
+
+		String[] stringList = {"a", "b", "c"};
+
+		Usuario[] usuarioList = { new Usuario("juan"), new Usuario() };
+
+
+
+		doubleList[0] = 11.0;
+		doubleList[2] = 21.0;
+		doubleList[4] = 31.0;
+		//myList[10] = 31;
+
+		//recorrer double
+		System.out.println(doubleList);
+		for( double num : doubleList) {
+
+			System.out.println(num);
+
+		}
+
+		//recorrer int
+		System.out.println(intList);
+		for( int numInt : intList) {
+
+			System.out.println(numInt);
+
+		}
+
+		//recorrer int
+		System.out.println(stringList);
+		for( String  s : stringList) {
+
+			System.out.println(s);
+
+		}
+
+		//recorrer Usuario
+		System.out.println(usuarioList);
+
+		System.out.println("\nLista de Usuarios");
+		for( Usuario usuario : usuarioList) {
+
+			System.out.println(usuario.getName() + " " + usuario.getAddress() + " " + usuario.getBirthDate());
+
+		}
 
 	}
 }
---
+++ src/main/java/bo/gob/asfi/tipos/model/Usuario.java
@@ -0,0 +1,61 @@
+package bo.gob.asfi.tipos.model;
+
+import java.util.Date;
+
+/**
+ * Created by fernando on 10/12/16.
+ */
+public class Usuario
+{
+	private String name;
+
+	private String address;
+
+	private Date birthDate;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+	public Date getBirthDate()
+	{
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate)
+	{
+		this.birthDate = birthDate;
+	}
+
+	public Usuario(String name)
+	{
+		this.name = name;
+		this.address = "address of " + name;
+		this.birthDate = new Date();
+	}
+
+	public Usuario()
+	{
+		this.name = null;
+		this.address = null;
+		this.birthDate = null;
+	}
+
+}
+
