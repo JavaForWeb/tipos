d7b208e3f0fd5b0765b5ddf24874654666ad7e18
Fernando Ontiveros <feronti@gmail.com>
2016-10-17 14:12:54 -0400

    inserts

---
+++ src/main/resources/fixtures/insert_cities.sql
@@ -0,0 +1,29 @@
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(5976,1,835,'Kabul','34.517','69.183','+04:50',0,'KABU');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(42230,1,852,'Mazar-e Sharif','36.7','67.1','+4:30',0,'MSHA');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(42412,2,983,'Korce','40.6162','20.7779','+01:00',0,'KORC');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(5977,2,1011,'Tirane','41.333','19.833','+01:00',0,'TIRA');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(5978,3,856,'Algiers','36.763','3.051','+01:00',0,'ALGI');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(5981,3,858,'Skikda','36.879','6.907','+01:00',0,'SKIK');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(5980,3,861,'Oran','35.691','-0.642','+01:00',0,'ORAN');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(42211,3,868,'Ouled Djellal','34.417','5.067','+01:00',0,'ODJE');
+
+INSERT INTO public."city" ("CityId", "CountryId", "RegionId", "City", "Latitude", "Longitude", "TimeZone", "DmaId", "CODE") VALUES
+(42231,1,833,'Herat',34.333,62.2,'+04:30',0,'HERA');
+
+
